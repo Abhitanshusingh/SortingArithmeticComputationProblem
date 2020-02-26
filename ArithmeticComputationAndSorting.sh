bf9a6eec00a7cc57a2f47e5353a94cec1ac31943
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-26 10:18:58 +0530

    MODIFIED: Added first arithmetic operation

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -3,3 +3,5 @@ echo "Welcome to arithmetic computation problem"
 read -p "Enter number: " a
 read -p "Enter number: " b
 read -p "Enter number: " c
+operation1=`echo "scale=4; $a + $b * $c" |bc -l`
+echo "a+b*c = $operation1"
