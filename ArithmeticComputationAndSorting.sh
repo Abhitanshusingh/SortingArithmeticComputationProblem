31710d3f6c4f513f0d433f3644467cbbc1831257
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-26 11:49:51 +0530

    MODIFIED: Storing dictionary value into a array

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -15,8 +15,10 @@ operation3=`echo "scale=4; $c + $a / $b" |bc -l`
 Arithmetic[3]="$operation3"
 operation4=`echo "scale=4; $a % $b + $c" |bc -l`
 Arithmetic[4]="$operation4"
-#Printing output and message
-echo "a+b*c = 1 : ${Arithmetic[1]} "
-echo "a*b+c = 2 : ${Arithmetic[2]} "
-echo "c+a/b = 3 : ${Arithmetic[3]} "
-echo "a%b+c = 4 : ${Arithmetic[4]} "
+#Storing dictionary value into array
+for iterator in ${!Arithmetic[@]}
+do
+	array[$iterator]="${Arithmetic[$iterator]}"
+done
+#Printing array value
+echo "Array value is : ${array[@]}"
