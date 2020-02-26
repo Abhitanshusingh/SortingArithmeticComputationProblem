9ee310b3f3f995c64356d92d01a6ee844e2bb2cc
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-26 12:17:08 +0530

    MODIFIED : Sorting array in descending order

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -6,6 +6,7 @@ declare -A Arithmetic
 read -p "Enter number: " a
 read -p "Enter number: " b
 read -p "Enter number: " c
+
 #Performing arithmetic operation
 operation1=`echo "scale=4; $a + $b * $c" |bc -l`
 Arithmetic[1]="$operation1"
@@ -15,10 +16,28 @@ operation3=`echo "scale=4; $c + $a / $b" |bc -l`
 Arithmetic[3]="$operation3"
 operation4=`echo "scale=4; $a % $b + $c" |bc -l`
 Arithmetic[4]="$operation4"
+
 #Storing dictionary value into array
 for iterator in ${!Arithmetic[@]}
 do
 	array[$iterator]="${Arithmetic[$iterator]}"
 done
+
 #Printing array value
 echo "Array value is : ${array[@]}"
+
+#Shorting array into descending order
+for(( i=0; i<${#array[@]}; i++ ))
+do
+	for (( j=$i+1 ; j<${#array[@]}; j++ ))
+	do
+		if(( `echo "array[i] < arra[j] == 0" |bc -q` ==1 ))
+		then
+			temp="${array[$i]}"
+			array[$i]="${array[$j]}"
+			array[$j]="$temp"
+		fi
+	done
+done
+echo "In decending order : ${array[@]}"
+
