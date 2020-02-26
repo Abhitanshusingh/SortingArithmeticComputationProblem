e248664ef747b8af3dad57ce5c9b3d31095ff4f4
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-26 10:32:16 +0530

    MODIFIED: Added debuggind mode and  performing second arithmetic operation

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,7 +1,13 @@
+#!/bin/bash -x
 #Printing message
 echo "Welcome to arithmetic computation problem"
+#Taking input from user
 read -p "Enter number: " a
 read -p "Enter number: " b
 read -p "Enter number: " c
-operation1=`echo "scale=4; $a + $b * $c" |bc -l`
+#Performing arithmetic operation
+operation1=`echo "scale=4; $a + $b * $c" |bc -l` 
+operation2=`echo "scale=4; $a * $b + $c" |bc -l`
+#Printing output and message
 echo "a+b*c = $operation1"
+echo "a*b+c = $operation2"
